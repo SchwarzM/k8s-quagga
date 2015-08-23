a1ad09cfa84272f8276f3a2389780e32a5ce4bd8
Marian Schwarz <scorp@sblk.de>
2015-08-23 19:51:45 +0200

    -- add homenet to run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,6 @@ set -ex
 ./k8s-quagga zebra
 
 # generate ospfd conf
-./k8s-quagga ospfd --Interface ${K8S_QUAGGA_INTERFACE} --RouterId ${K8S_QUAGGA_ROUTERID} --PortalNet ${K8S_QUAGGA_PORTALNET} --ContainerNet ${K8S_QUAGGA_CONTAINERNET}
+./k8s-quagga ospfd --Interface ${K8S_QUAGGA_INTERFACE} --RouterId ${K8S_QUAGGA_ROUTERID} --HomeNet ${K8S_QUAGGA_HOMENET} --PortalNet ${K8S_QUAGGA_PORTALNET} --ContainerNet ${K8S_QUAGGA_CONTAINERNET}
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
